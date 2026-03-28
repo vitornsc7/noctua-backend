8807a73efdbc8586888f68c7d0bdf9897394c603
isabelaspaz <isabela.s.paz@gmail.com>
2026-03-28 16:07:26 -0300

    feat: adiciona UsuarioEntity

---
+++ src/main/java/com/noctua/backend/entity/Usuario/UsuarioEntity.java
@@ -0,0 +1,50 @@
+package com.noctua.backend.entity.Usuario;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Inheritance(strategy= InheritanceType.JOINED)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senhaHash;
+
+    @Column(nullable=false)
+    private Boolean ativo;
+
+    @Column
+    private String tokenSenhaReset;
+
+    @Column
+    private LocalDateTime tokenSenhaExpiracao;
+
+}
