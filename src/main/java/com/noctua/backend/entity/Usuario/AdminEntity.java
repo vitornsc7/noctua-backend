db2de64284deea9667f9f67703e3aa2f38a5bff3
isabelaspaz <isabela.s.paz@gmail.com>
2026-04-01 22:09:51 -0300

    refactor: ajusta modelagem de usuario, admin e professor para relacionamento one-to-one

--- src/main/java/com/noctua/backend/entity/Usuario/AdminEntity.java
+++ src/main/java/com/noctua/backend/entity/Usuario/AdminEntity.java
@@ -1,6 +1,10 @@
 package com.noctua.backend.entity.Usuario;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +17,13 @@ import lombok.Setter;
 @NoArgsConstructor
 
 
-public class AdminEntity extends UsuarioEntity{
+public class AdminEntity {
     
+    @Id
+    private Long id;
+
+    @OneToOne(optional=false, cascade = jakarta.persistence.CascadeType.ALL)
+    @MapsId
+    @JoinColumn(name = "id")
+    private UsuarioEntity usuario;
 }
--- src/main/java/com/noctua/backend/entity/Usuario/UsuarioEntity.java
+++ src/main/java/com/noctua/backend/entity/Usuario/UsuarioEntity.java
@@ -7,21 +7,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "usuarios")
-@Inheritance(strategy= InheritanceType.JOINED)
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 
 public class UsuarioEntity {
 
@@ -38,7 +33,7 @@ public class UsuarioEntity {
     @Column(nullable = false)
     private String senhaHash;
 
-    @Column(nullable=false)
+    @Column(nullable = false)
     private Boolean ativo;
 
     @Column
